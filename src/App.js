43984d3accaaf10a68a81f0035ca046da17418bb
HipsterSantos <santoscampos269@gmail.com>
2021-05-26 11:02:10 +0100

    adding graph element

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Graph from './Graph';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div style={styles}>
+    <Graph name="CodeSandbox" />
+    <h2>Start editing to see some magic happen {'\u2728'}</h2>
+  </div>
   );
 }
 
